0319a21769a319d0ab06715e7307b872299c1256
BruceBanner <afoninvladimir@yahoo.com>
2019-04-05 23:39:30 +0300

    - added composer autoload

--- api/config/config.php
+++ api/config/config.php
@@ -1,7 +1,14 @@
 <?php
 
+use Api\Http\Action\HomeAction;
+
 return [
     'settings' => [
         'addContentLengthHeader' => false,
     ],
+
+    HomeAction::class => function() {
+        return new HomeAction();
+    }
+
 ];
--- api/public/index.php
+++ api/public/index.php
@@ -8,15 +8,16 @@ echo json_encode([
 ]);*/
 
 use Api\Http\Action;
-
+use Slim\App;
 
 chdir(dirname(__DIR__));
 
+require 'vendor/autoload.php';
 $config = require 'config/config.php';
 
-$app = new \Slim\App($config);
+$container = new \Slim\Container($config);
+$app = new App($container);
 
 $app->get('/', Action\HomeAction::class);
-
 
 $app->run();
