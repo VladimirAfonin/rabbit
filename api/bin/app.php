f56578181159923ad0fdb26261a6056fa5d5ff10
BruceBanner <afoninvladimir@yahoo.com>
2019-06-23 13:41:02 +0300

    - fix doctrine/migrations & add user migration

--- api/bin/app.php
+++ api/bin/app.php
@@ -1,18 +1,17 @@
+#!/usr/bin/env php
 <?php
 declare(strict_types=1);
 
+use Doctrine\Migrations\Tools\Console\Helper\ConfigurationHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Dotenv\Dotenv;
-use Doctrine\Migrations\Tools\Console\Helper\ConfigurationHelper;
 use Doctrine\Migrations\Configuration\Configuration;
-use Doctrine\ORM\Tools\Console\ConsoleRunner as OrmConsoleRunner;
-use Doctrine\DBAL\Tools\Console\ConsoleRunner;
+use Doctrine\Migrations\Tools\Console\ConsoleRunner;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
-
 if (file_exists('.env')) {
     (new Dotenv())->load('.env');
 }
@@ -32,12 +31,11 @@ $configuration->setMigrationsNamespace('Api\Data\Migration');
 $cli->getHelperSet()->set(new EntityManagerHelper($entityManager), 'em');
 $cli->getHelperSet()->set(new ConfigurationHelper($connection, $configuration), 'configuration');
 
-OrmConsoleRunner::addCommands($cli);
+Doctrine\ORM\Tools\Console\ConsoleRunner::addCommands($cli);
 ConsoleRunner::addCommands($cli);
 
 $commands = $container->get('config')['console']['commands'];
 foreach ($commands as $command) {
     $cli->add($container->get($command));
 }
-
 $cli->run();
