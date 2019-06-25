68062da88105edcee1249188220c9b0e56327e88
BruceBanner <afoninvladimir@yahoo.com>
2019-06-25 09:14:41 +0300

    - added test cases & test env

--- api/tests/Feature/WebTestCase.php
+++ api/tests/Feature/WebTestCase.php
@@ -3,15 +3,19 @@ declare(strict_types=1);
 
 namespace Api\Test\Feature;
 
-use PHPUnit\Framework\TestCase;
-use Psr\Container\ContainerInterface;
-use Psr\Http\Message\{ResponseInterface, ServerRequestInterface};
-use Slim\App;
-use Zend\Diactoros\{Response, ServerRequest, Uri};
 use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
 use Doctrine\Common\DataFixtures\Loader;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+use Zend\Diactoros\Response;
+use Zend\Diactoros\ServerRequest;
+use Zend\Diactoros\Stream;
+use Zend\Diactoros\Uri;
 
 class WebTestCase extends TestCase
 {
@@ -20,22 +24,31 @@ class WebTestCase extends TestCase
         return $this->method($uri, 'GET');
     }
 
-    /**
-     * @param string $uri
-     * @param $method
-     * @return ResponseInterface
-     * @throws \Slim\Exception\MethodNotAllowedException
-     * @throws \Slim\Exception\NotFoundException
-     */
-    protected function method(string $uri, $method)
+
+    protected function post(string $uri, array $params = []): ResponseInterface
     {
+        return $this->method($uri, 'POST', $params);
+    }
+    protected function method(string $uri, $method, array $params = []): ResponseInterface
+    {
+        $body = new Stream('php://temp', 'r+');
+        $body->write(json_encode($params));
+        $body->rewind();
         return $this->request(
             (new ServerRequest())
+                ->withHeader('Content-Type', 'application/json')
+                ->withHeader('Accept', 'application/json')
                 ->withUri(new Uri('http://test' . $uri))
                 ->withMethod($method)
+                ->withBody($body)
         );
     }
-
+    protected function request(ServerRequestInterface $request): ResponseInterface
+    {
+        $response = $this->app()->process($request, new Response());
+        $response->getBody()->rewind();
+        return $response;
+    }
     protected function loadFixtures(array $fixtures): void
     {
         $container = $this->container();
@@ -52,20 +65,6 @@ class WebTestCase extends TestCase
         $executor = new ORMExecutor($em, new ORMPurger($em));
         $executor->execute($loader->getFixtures());
     }
-
-    /**
-     * @param ServerRequestInterface $request
-     * @return ResponseInterface
-     * @throws \Slim\Exception\MethodNotAllowedException
-     * @throws \Slim\Exception\NotFoundException
-     */
-    protected function request($request): ResponseInterface
-    {
-        $response = $this->app()->process($request, new Response());
-        $response->getBody()->rewind();
-        return $response;
-    }
-
     private function app(): App
     {
         $container = $this->container();
@@ -73,7 +72,6 @@ class WebTestCase extends TestCase
         (require 'config/routes.php')($app);
         return $app;
     }
-
     private function container(): ContainerInterface
     {
         return require 'config/container.php';
