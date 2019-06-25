6979cd5e1b3c6570c6534db1e19adc5e46cfcdae
BruceBanner <afoninvladimir@yahoo.com>
2019-06-25 09:59:03 +0300

    - added validation

--- api/config/common/app.php
+++ api/config/common/app.php
@@ -5,8 +5,19 @@ use Api\Http\Action;
 use Api\Model;
 use Psr\Container\ContainerInterface;
 use Api\Http\Middleware;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 return [
+    ValidatorInterface::class => function () {
+        AnnotationRegistry::registerLoader('class_exists');
+        return Validation::createValidatorBuilder()
+            ->enableAnnotationMapping()
+            ->getValidator();
+    },
+
+
     Middleware\DomainExceptionMiddleware::class => function () {
         return new Middleware\DomainExceptionMiddleware();
     },
@@ -17,13 +28,16 @@ return [
 
     Action\Auth\SignUp\RequestAction::class => function (ContainerInterface $container) {
         return new Action\Auth\SignUp\RequestAction(
-            $container->get(Model\User\UseCase\SignUp\Request\Handler::class)
+            $container->get(Model\User\UseCase\SignUp\Request\Handler::class),
+            $container->get(ValidatorInterface::class)
+
         );
     },
 
     Action\Auth\SignUp\ConfirmAction::class => function (ContainerInterface $container) {
         return new Action\Auth\SignUp\ConfirmAction(
-            $container->get(Model\User\UseCase\SignUp\Confirm\Handler::class)
+            $container->get(Model\User\UseCase\SignUp\Confirm\Handler::class),
+            $container->get(ValidatorInterface::class)
         );
     },
 ];
