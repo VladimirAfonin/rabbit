612cf7d183514319d766a57cd8e118eac5a8be4b
BruceBanner <afoninvladimir@yahoo.com>
2019-06-29 17:41:27 +0300

    - added video usecases

---
+++ api/src/Model/Video/Entity/Video/Size.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Api\Model\Video\Entity\Video;
+
+class Size
+{
+    private $width;
+    private $height;
+
+    public function __construct(int $width, int $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function getWidth(): int
+    {
+        return $this->width;
+    }
+
+    public function getHeight(): int
+    {
+        return $this->height;
+    }
+}
---
+++ api/src/Model/Video/UseCase/Video/Remove/Command.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Api\Model\Video\UseCase\Video\Remove;
+
+class Command
+{
+    public $id;
+}
