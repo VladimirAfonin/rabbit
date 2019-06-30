902c4e2f9bd520b5d15f654b3882d5d9265f6100
BruceBanner <afoninvladimir@yahoo.com>
2019-06-30 19:55:01 +0300

    - added video mapping

--- api/src/Model/Video/Entity/Video/Size.php
+++ api/src/Model/Video/Entity/Video/Size.php
@@ -3,9 +3,23 @@ declare(strict_types=1);
 
 namespace Api\Model\Video\Entity\Video;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Embeddable
+ */
 class Size
 {
+    /**
+     * @var int
+     * @ORM\Column(type="integer", nullable=true)
+     */
     private $width;
+
+    /**
+     * @var int
+     * @ORM\Column(type="integer", nullable=true)
+     */
     private $height;
 
     public function __construct(int $width, int $height)
